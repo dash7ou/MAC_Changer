d82e62b80af9d517f54e093ae046466c3606a752
dashzou <fxfddfno@gmail.com>
2020-06-29 04:37:49 -0400

    more secure code for user input

--- mac_changer.py
+++ mac_changer.py
@@ -1,11 +1,13 @@
 # !/usr/bin/env python
-
 import subprocess
 
+# if you want to work with pyton2 insted of input using raw_input
 interface = input("Enter your interface you need to change its MAC > ")
 new_mac = input("Enter new MAC > ")
+
 print("[+] Change MAC address for " + interface + " to " + new_mac)
 
-subprocess.call("ifconfig "+ interface +" down", shell=True)
-subprocess.call("ifconfig " + interface +" hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig "+ interface +" up", shell=True)
+# this way is more secure than using string, you can not run commant we did not want to excute
+subprocess.call(['ifconfig', interface, "down"])
+subprocess.call(['ifconfig', interface, 'hw', 'ether', new_mac])
+subprocess.call(['ifconfig', interface, 'up'])
