4debd66e100c06a7f39d43408e4ca7ed4515918a
dashzou <fxfddfno@gmail.com>
2020-06-29 05:04:50 -0400

    using optparse module

--- mac_changer.py
+++ mac_changer.py
@@ -1,13 +1,25 @@
 # !/usr/bin/env python
 import subprocess
+import optparse
 
-# if you want to work with pyton2 insted of input using raw_input
-interface = input("Enter your interface you need to change its MAC > ")
-new_mac = input("Enter new MAC > ")
 
-print("[+] Change MAC address for " + interface + " to " + new_mac)
+# change make function
+def change_mac(interface, new_mac):
+    print("[+] Change MAC address for " +
+          interface + " to " + new_mac)
 
-# this way is more secure than using string, you can not run commant we did not want to excute
-subprocess.call(['ifconfig', interface, "down"])
-subprocess.call(['ifconfig', interface, 'hw', 'ether', new_mac])
-subprocess.call(['ifconfig', interface, 'up'])
+    # this way is more secure than using string, you can not run commant we did not want to excute
+    subprocess.call(['ifconfig', interface, "down"])
+    subprocess.call(['ifconfig', interface,
+                     'hw', 'ether', new_mac])
+    subprocess.call(['ifconfig', interface, 'up'])
+
+
+parser = optparse.OptionParser()
+parser.add_option("-i", "--interface", dest="interface",
+                  help="An interface to change its MAC address.")
+parser.add_option("-m", "--mac", dest="new_mac",
+                  help="A new mac address to change old address")
+
+(options, arguments) = parser.parse_args()
+change_mac(options.interface, options.new_mac)
